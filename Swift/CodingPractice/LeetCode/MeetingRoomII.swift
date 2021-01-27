2fcf77f21a223983f708d2aaca2c5c6ad539ed44
blackkynaja <blackkynaja@gmail.com>
2021-01-26 17:55:32 -0800

    Add Leetcode practices

---
+++ Swift/CodingPractice/LeetCode/MeetingRoomII.swift
@@ -0,0 +1,27 @@
+/*
+ https://leetcode.com/problems/meeting-rooms-ii/
+ */
+
+func minMeetingRooms(_ intervals: [[Int]]) -> Int {
+    var starts = intervals.map { (items) -> Int in
+        return items[0]
+    }
+    starts = starts.sorted()
+    
+    var ends = intervals.map { (items) -> Int in
+        return items[1]
+    }
+    ends = ends.sorted()
+    
+    var minRoom = 0
+    var endIndex = 0
+    for start in starts {
+        if start < ends[endIndex] {
+            minRoom+=1
+        } else {
+            endIndex+=1
+        }
+    }
+    
+    return minRoom
+}
---
+++ Swift/CodingPractice/LeetCode/Permutations.swift
@@ -0,0 +1,25 @@
+/*
+ https://leetcode.com/problems/permutations/
+ */
+
+private func permuteHelper(_ nums: [Int],_ temps: inout [Int],_ results: inout [[Int]]) {
+    if temps.count == nums.count {
+        results.append(temps)
+        return
+    }
+    
+    for num in nums {
+        if !temps.contains(num) {
+            temps.append(num)
+            permuteHelper(nums, &temps, &results)
+            temps.removeLast()
+        }
+    }
+}
+
+func permute(_ nums: [Int]) -> [[Int]] {
+    var temps = [Int]()
+    var results = [[Int]]()
+    permuteHelper(nums, &temps, &results)
+    return results
+}
