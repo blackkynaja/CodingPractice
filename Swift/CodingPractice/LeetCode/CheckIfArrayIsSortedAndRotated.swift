0884fba47c019d647dfcccf95b62b9563004efca
blackkynaja <blackkynaja@gmail.com>
2021-02-06 23:16:35 -0800

    Add Check if Array Is Sorted and Rotated

---
+++ Swift/CodingPractice/LeetCode/CheckIfArrayIsSortedAndRotated.swift
@@ -0,0 +1,32 @@
+//
+//  CheckIfArrayIsSortedAndRotated.swift
+//  Test
+//
+//  Created by Yutthapong Kawunruan on 2/6/21.
+//
+
+func check(_ nums: [Int]) -> Bool {
+    var rotIndex = -1
+    
+    for i in 1..<nums.count {
+        if nums[i-1] > nums[i] {
+            rotIndex = i
+            break
+        }
+    }
+    
+    if rotIndex == -1 {
+        return true
+    }
+    
+    for i in 0..<nums.count-1 {
+        let index = (i+rotIndex)%nums.count
+        let nextIndex = (index+1)%nums.count
+        print(nums[index], nums[nextIndex])
+        if nums[index] > nums[nextIndex] {
+            return false
+        }
+    }
+    
+    return true
+}
