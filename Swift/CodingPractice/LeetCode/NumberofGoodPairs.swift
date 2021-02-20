9556df1f02b45b939e8aa22868f2028043f0a8c3
blackkynaja <blackkynaja@gmail.com>
2021-02-20 15:41:45 -0800

    Add Number of Good Pairs

---
+++ Swift/CodingPractice/LeetCode/NumberofGoodPairs.swift
@@ -0,0 +1,18 @@
+/*
+ https://leetcode.com/problems/number-of-good-pairs/
+ */
+
+func numIdenticalPairs(_ nums: [Int]) -> Int {
+    var dict = [Int:Int]()
+    var count = 0
+    for num in nums {
+        if let value = dict[num] {
+            count+=value
+            dict[num]!+=1
+        } else {
+            dict[num] = 1
+        }
+    }
+    
+    return count
+}
