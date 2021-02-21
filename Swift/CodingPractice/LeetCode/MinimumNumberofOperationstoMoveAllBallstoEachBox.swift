35aa36d2beae91bf9610ba10fb3df4218143a264
blackkynaja <blackkynaja@gmail.com>
2021-02-20 23:47:42 -0800

    Add K Closest Points to Origin

---
+++ Swift/CodingPractice/LeetCode/MinimumNumberofOperationstoMoveAllBallstoEachBox.swift
@@ -0,0 +1,23 @@
+/*
+ 
+ */
+
+func minOperations(_ boxes: String) -> [Int] {
+    var stones = [Int]()
+    for (index,box) in boxes.enumerated() {
+        if box == "1" {
+            stones.append(index)
+        }
+    }
+    
+    var results = [Int]()
+    for (index, _) in boxes.enumerated() {
+        var count = 0
+        for stone in stones {
+            count+=abs(index-stone)
+        }
+        results.append(count)
+    }
+    
+    return results
+}
