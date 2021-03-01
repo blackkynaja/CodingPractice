1f96044a47ed345d2ac5b6b97f2106f4ba7d0463
blackkynaja <blackkynaja@gmail.com>
2021-03-01 15:29:06 -0800

    Add MinimumChangesToMakeAlternatingBinaryString

---
+++ Swift/CodingPractice/LeetCode/MinimumChangesToMakeAlternatingBinaryString.swift
@@ -0,0 +1,27 @@
+/*
+ https://leetcode.com/problems/minimum-changes-to-make-alternating-binary-string/
+ */
+
+func minOperations(_ s: String) -> Int {
+    var matchPattern1 = 0
+    var matchPattern2 = 0
+    let arrS = Array(s)
+    
+    for (index, value) in arrS.enumerated() {
+        if index%2==0 {
+            if value == "0" {
+                matchPattern1+=1
+            } else {
+                matchPattern2+=1
+            }
+        } else {
+            if value == "0" {
+                matchPattern2+=1
+            } else {
+                matchPattern1+=1
+            }
+        }
+    }
+    
+    return min(matchPattern1, matchPattern2)
+}
