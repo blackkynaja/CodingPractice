e4a9c1c52e890567a8566e7c3b54f6af225b69c2
blackkynaja <blackkynaja@gmail.com>
2021-03-09 21:21:26 -0800

    Create ThreeSum.swift

---
+++ Swift/CodingPractice/LeetCode/ThreeSum.swift
@@ -0,0 +1,31 @@
+/*
+ https://leetcode.com/problems/3sum/
+ */
+class ThreeSum {
+    func threeSum(_ nums: [Int]) -> [[Int]] {
+        var results = [[Int]]()
+        var visited = Set<Int>()
+        let sortedNums = nums.sorted()
+        for (firstIndex, firstValue) in sortedNums.enumerated() {
+            if visited.contains(firstValue) {
+                continue
+            }
+            let target = 0 - firstValue
+            var set = Set<Int>()
+            var insideVisited = Set<Int>()
+            for i in firstIndex+1..<sortedNums.count {
+                print(firstValue, sortedNums[i], set)
+                if set.contains(target-sortedNums[i]) && !insideVisited.contains(sortedNums[i]) {
+                    results.append([firstValue, sortedNums[i], target-sortedNums[i]])
+                    insideVisited.insert(sortedNums[i])
+                    insideVisited.insert(target-sortedNums[i])
+                } else {
+                    set.insert(sortedNums[i])
+                }
+            }
+            visited.insert(firstValue)
+        }
+        
+        return results
+    }
+}
