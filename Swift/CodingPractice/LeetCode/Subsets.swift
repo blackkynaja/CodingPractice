8bf03cd592a711b4d308e027af2c05c2f13d3bb0
blackkynaja <blackkynaja@gmail.com>
2021-03-09 21:21:17 -0800

    Create Subsets.swift

---
+++ Swift/CodingPractice/LeetCode/Subsets.swift
@@ -0,0 +1,31 @@
+/*
+ https://leetcode.com/problems/subsets/
+ */
+
+class Subsets {
+    func subsetsHelper(_ nums: [Int],_ startIndex: Int,_ set: [Int], _ results: inout [[Int]]) {
+        results.append(set)
+        if startIndex == nums.count {
+            return
+        }
+        for i in startIndex..<nums.count {
+            let temp = set+[nums[i]]
+            subsetsHelper(nums, i+1, temp, &results)
+        }
+    }
+    
+    func subsets(_ nums: [Int]) -> [[Int]] {
+        var results = [[Int]]()
+        results.append([])
+        for i in 0..<nums.count {
+            subsetsHelper(nums, i+1, [nums[i]], &results)
+        }
+        
+        return results
+    }
+}
+
+
+class Solution {
+    
+}
