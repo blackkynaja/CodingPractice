efe20dd1832812be659535dcd91919b9cc314cc4
blackkynaja <blackkynaja@gmail.com>
2021-03-09 21:21:04 -0800

    Update MergeTwoBinaryTrees.swift

--- Swift/CodingPractice/LeetCode/MergeTwoBinaryTrees.swift
+++ Swift/CodingPractice/LeetCode/MergeTwoBinaryTrees.swift
@@ -2,43 +2,44 @@
  https://leetcode.com/problems/merge-two-binary-trees/
  */
 
-//Definition for a binary tree node.
-public class TreeNode {
-    public var val: Int
-    public var left: TreeNode?
-    public var right: TreeNode?
-    public init() { self.val = 0; self.left = nil; self.right = nil; }
-    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-        self.val = val
-        self.left = left
-        self.right = right
-    }
-}
-
-func mergeTreesHelper(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
-    guard let leftNode = root1 else {
-        return root2 ?? nil
+class MergeTwoBinaryTrees {
+    class TreeNode {
+        public var val: Int
+        public var left: TreeNode?
+        public var right: TreeNode?
+        public init() { self.val = 0; self.left = nil; self.right = nil; }
+        public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+        public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+            self.val = val
+            self.left = left
+            self.right = right
+        }
     }
     
-    guard let rightNode = root2 else {
-        return leftNode
+    func mergeTreesHelper(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        guard let leftNode = root1 else {
+            return root2 ?? nil
+        }
+        
+        guard let rightNode = root2 else {
+            return leftNode
+        }
+        
+        let tree = TreeNode()
+        tree.val = leftNode.val+rightNode.val
+        if (leftNode.left != nil) || (rightNode.left != nil) {
+            tree.left = mergeTrees(leftNode.left, rightNode.left)
+        }
+        if (leftNode.right != nil) || (rightNode.right != nil) {
+            tree.right = mergeTrees(leftNode.right, rightNode.right)
+        }
+        return tree
     }
-    
-    let tree = TreeNode()
-    tree.val = leftNode.val+rightNode.val
-    if (leftNode.left != nil) || (rightNode.left != nil) {
-        tree.left = mergeTrees(leftNode.left, rightNode.left)
-    }
-    if (leftNode.right != nil) || (rightNode.right != nil) {
-        tree.right = mergeTrees(leftNode.right, rightNode.right)
-    }
-    return tree
-}
 
-func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
-    let tree = mergeTreesHelper(root1, root2) ?? nil
-    return tree
+    func mergeTrees(_ root1: TreeNode?, _ root2: TreeNode?) -> TreeNode? {
+        let tree = mergeTreesHelper(root1, root2) ?? nil
+        return tree
+    }
 }
  
  
