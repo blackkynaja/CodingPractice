8417fa7bd92bf05f3c7a7c85ad5e8ca51a88fb3d
blackkynaja <blackkynaja@gmail.com>
2021-04-13 23:53:59 -0700

    Create IntersectionofTwoLinkedLists.swift

---
+++ Swift/CodingPractice/LeetCode/IntersectionofTwoLinkedLists.swift
@@ -0,0 +1,57 @@
+/*
+ https://leetcode.com/problems/intersection-of-two-linked-lists/submissions/
+ */
+
+class IntersectionofTwoLinkedLists {
+    // Definition for singly-linked list.
+    class ListNode {
+        public var val: Int
+        public var next: ListNode?
+        public init(_ val: Int) {
+            self.val = val
+            self.next = nil
+        }
+    }
+    
+    func getLength(_ head: ListNode?) -> Int {
+        var length = 0
+        var temp = head
+        while (temp != nil) {
+            temp = temp!.next
+            length+=1
+        }
+        
+        return length
+    }
+    
+    func getIntersectionNode(_ headA: ListNode?, _ headB: ListNode?) -> ListNode? {
+        if (headA == nil || headB == nil) {
+            return nil
+        }
+        
+        var lenA = getLength(headA)
+        var lenB = getLength(headB)
+        var currentA = headA
+        var currentB = headB
+        
+        while (lenA > lenB) {
+            lenA-=1
+            currentA = currentA!.next
+        }
+        while (lenA < lenB) {
+            lenB-=1
+            currentB = currentB!.next
+        }
+        
+        while (currentA !== currentB) {
+            if (currentA == nil || currentB == nil) {
+                return nil
+            }
+            
+            currentA = currentA!.next
+            currentB = currentB!.next
+        }
+        
+        return currentA
+    }
+}
