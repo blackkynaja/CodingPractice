eed2584d308f465d802516fc109c7efc9ebd3f48
blackkynaja <blackkynaja@gmail.com>
2021-06-01 23:23:47 -0700

    Update LonelyInteger.swift

--- Swift/CodingPractice/HackerRank/LonelyInteger.swift
+++ Swift/CodingPractice/HackerRank/LonelyInteger.swift
@@ -2,7 +2,7 @@
  https://www.hackerrank.com/challenges/lonely-integer/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=7-day-campaign
  */
 
-class Solution {
+class LonelyInteger {
     func lonelyinteger(a: [Int]) -> Int {
         var set = Set<Int>()
         
