05e9ab7056a137c108f3596ca8a52c8b8d89bf91
blackkynaja <blackkynaja@gmail.com>
2021-10-23 22:05:26 -0700

    Create RobotBoundedInCircle.swift

---
+++ Swift/CodingPractice/LeetCode/RobotBoundedInCircle.swift
@@ -0,0 +1,37 @@
+/*
+ https://leetcode.com/problems/robot-bounded-in-circle/
+ */
+
+class RobotBoundedInCircle {
+    func isRobotBounded(_ instructions: String) -> Bool {
+        // GGLGRGRGRG
+        var direction = 0
+        var pos = (0,0)
+        let move = [(0,1),(1,0),(0,-1),(-1,0)]
+        var i = 1
+        
+        while i <= 4 {
+            for s in instructions {
+                if s == "G" {
+                    pos.0+=move[direction].0
+                    pos.1+=move[direction].1
+                } else {
+                    if s == "L" {
+                        direction-=1
+                    } else if s == "R" {
+                        direction+=1
+                    }
+                    if direction == -1 {
+                    direction = 3
+                    }
+                    direction = direction%4
+                }
+            }
+            i+=1
+            if pos == (0,0) {
+                return true
+            }
+        }
+        return false
+    }
+}
