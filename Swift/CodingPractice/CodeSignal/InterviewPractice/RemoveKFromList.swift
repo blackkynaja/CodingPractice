bfb4d3b1fdaca6c44a87de3281cd1e9a20213ccf
blackkynaja <blackkynaja@gmail.com>
2021-12-29 23:58:50 -0800

    Update RemoveKFromList.swift

--- Swift/CodingPractice/CodeSignal/InterviewPractice/RemoveKFromList.swift
+++ Swift/CodingPractice/CodeSignal/InterviewPractice/RemoveKFromList.swift
@@ -3,22 +3,18 @@
  */
 
 class RemoveKFromList {
-    class ListNode<T> {
-        let value: T? = nil
-        var next: ListNode<T>? = nil
-    }
-
-    func removeKFromList(l: ListNode<Int>?, k: Int) -> ListNode<Int>? {
+    // MARK: - Solution1
+    func removeKFromList(l: ListNode?, k: Int) -> ListNode? {
         var current = l
-        var temp = current?.value
-        var prev: ListNode<Int>?
-        var newCurr: ListNode<Int>?
+        var temp = current?.val
+        var prev: ListNode?
+        var newCurr: ListNode?
         while current != nil && temp == k {
-            if current?.value == k {
+            if current?.val == k {
                 newCurr = current?.next
             }
             current = current?.next
-            temp = current?.value
+            temp = current?.val
         }
 
         if let new = newCurr {
@@ -28,7 +24,7 @@ class RemoveKFromList {
         }
         prev = current
         while current != nil {
-            if current?.value == k {
+            if current?.val == k {
                 prev?.next = current?.next
             } else {
                 prev = current
@@ -37,5 +33,21 @@ class RemoveKFromList {
 
         }
         return newCurr
+    }
+    
+    // MARK: - Solution2
+    func removeElements1(_ head: ListNode?, _ val: Int) -> ListNode? {
+        if head == nil { return head }
+        
+        var current = head
+        while current!.next != nil {
+            if current!.next!.val == val {
+                current!.next = current!.next!.next
+            }  else {
+                current = current!.next
+            }
+        }
+        
+        return head!.val == val ? head!.next : head
     }
 }
