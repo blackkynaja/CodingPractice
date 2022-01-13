1a4e44c2afa2e16e4150842ef240251b0b6f5f13
blackkynaja <blackkynaja@gmail.com>
2022-01-12 22:58:39 -0800

    Update MinimumNumberofArrowstoBurstBalloons.swift

--- Swift/CodingPractice/LeetCode/MinimumNumberofArrowstoBurstBalloons.swift
+++ Swift/CodingPractice/LeetCode/MinimumNumberofArrowstoBurstBalloons.swift
@@ -5,8 +5,14 @@
  */
 
 class MinimumNumberofArrowstoBurstBalloons {
-    func findMinArrowShots(_ points: [[Int]]) -> Int {
-        var sorted = points.sorted {$0[0] < $1[0]}
+    
+    // MARK: - Find Interception
+    /*
+     - Sort array by starting point
+     - Find interception between to neigbour ballon by merge the interception to the merge array
+     */
+    func findMinArrowShots1(_ points: [[Int]]) -> Int {
+        let sorted = points.sorted {$0[0] < $1[0]}
         var merged = [sorted.first!]
         
         for i in 1..<sorted.count {
@@ -21,5 +27,31 @@ class MinimumNumberofArrowstoBurstBalloons {
         }
         
         return merged.count
+    }
+    
+    // MARK: - Greedy
+    /*
+     - Sort array by ending point
+     - Loop through sorted array if the next balloon start after the ending of the least end point, increase count by 1 and change the end point
+     */
+    func findMinArrowShots2(_ points: [[Int]]) -> Int {
+        let sorted = points.sorted {$0[1] < $1[1]}
+        var end = sorted.first!
+        var count = 1
+        for i in 1..<sorted.count {
+            let next = sorted[i]
+            
+            if next[0] > end[1] {
+                end = next
+                count+=1
+            }
+        }
+        
+        return count
+    }
+    
+    // MARK: - Solution
+    func findMinArrowShots(_ points: [[Int]]) -> Int {
+        return findMinArrowShots1(points)
     }
 }
