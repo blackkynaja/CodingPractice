48c284504df07a3e880dced3924f2e05fdc61e12
blackkynaja <blackkynaja@gmail.com>
2022-01-26 00:01:22 -0800

    Update MaximumSubarray.swift

--- Swift/CodingPractice/LeetCode/MaximumSubarray.swift
+++ Swift/CodingPractice/LeetCode/MaximumSubarray.swift
@@ -1,8 +1,12 @@
 /*
  https://leetcode.com/problems/maximum-subarray/
+ 
+ Topics: Array, Divide and Conquer, DP
  */
 
 class MaximumSubarray {
+    
+    // MARK: - Solution 1
     func maxSubArrayCulmulative(_ nums: [Int]) -> Int {
         var sumArr = [0]
         
@@ -10,22 +14,19 @@ class MaximumSubarray {
             sumArr.append(sumArr.last!+n)
         }
         
-        var min = 0
-        var max = nums.first!
+        var minNum = 0
+        var maxNum = nums.first!
         for (index, value) in sumArr.enumerated() {
             if index > 0 {
-                if value - min > max {
-                    max = value - min
-                }
-                if value < min {
-                    min = value
-                }
+                maxNum = max(maxNum, value - minNum)
+                minNum = min(minNum, value)
             }
         }
         
-        return max
+        return maxNum
     }
     
+    // MARK: - Solution 2
     func maxSubArrayDP(_ nums: [Int]) -> Int {
         var dp = [nums.first!]
         var result = dp.first!
@@ -39,6 +40,24 @@ class MaximumSubarray {
         return result
     }
     
+    // MARK: - Solution 3
+    func maxSubArrayPrev(_ nums: [Int]) -> Int {
+        if nums.count == 0 {
+            return 0
+        }
+        var result = nums.first!
+        var prev = result > 0 ? result : 0
+        
+        for i in 1..<nums.count {
+            let next = prev > 0 ? prev+nums[i] : nums[i]
+            result = max(next, result)
+            prev = next > 0 ? next : 0
+        }
+        
+        return result
+    }
+    
+    // MARK: - Solution
     func maxSubArray(_ nums: [Int]) -> Int {
         return maxSubArrayCulmulative(nums)
     }
