fa98c45bf354f780fd58eb2c38fc48447eb5e9f8
blackkynaja <blackkynaja@gmail.com>
2022-01-26 00:02:10 -0800

    Update MaximumScoreFromRemovingStones.swift

--- Swift/CodingPractice/LeetCode/MaximumScoreFromRemovingStones.swift
+++ Swift/CodingPractice/LeetCode/MaximumScoreFromRemovingStones.swift
@@ -1,8 +1,11 @@
 /*
  https://leetcode.com/problems/maximum-score-from-removing-stones/
+ 
+ Topics: Greedy, Heap
  */
+
 class MaximumScoreFromRemovingStones {
-    func maximumScore(_ a: Int, _ b: Int, _ c: Int) -> Int {
+    func maximumScore1(_ a: Int, _ b: Int, _ c: Int) -> Int {
         var nums = [a,b,c]
         nums.sort(by: {$0 > $1})
         var count = 0
@@ -14,5 +17,15 @@ class MaximumScoreFromRemovingStones {
         }
 
         return count
+    }
+    
+    func maximumScore2(_ a: Int, _ b: Int, _ c: Int) -> Int {
+        var nums = [a,b,c]
+        nums.sort(by: {$0 < $1})
+        if nums[0]+nums[1] <= nums[2] {
+            return nums[0]+nums[1]
+        } else {
+            return nums[2]+(((nums[0]+nums[1])-nums[2])/2)
+        }
     }
 }
