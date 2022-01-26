3b875be694ec87eee60d68796405b61d444b2692
blackkynaja <blackkynaja@gmail.com>
2022-01-25 23:59:37 -0800

    Update LetterCombinationsOfAPhoneNumber.swift

--- Swift/CodingPractice/LeetCode/LetterCombinationsOfAPhoneNumber.swift
+++ Swift/CodingPractice/LeetCode/LetterCombinationsOfAPhoneNumber.swift
@@ -1,6 +1,9 @@
 /*
  https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+ 
+ Topics: Hash Table, String, Backtracking
  */
+
 class LetterCombinationsOfAPhoneNumber {
     func letterCombinations(_ digits: String) -> [String] {
         let letterComb = [2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wyxz"]
