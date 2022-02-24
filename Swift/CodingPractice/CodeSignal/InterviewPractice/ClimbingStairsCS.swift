98e2e4be8584db8b830753fec3eea5704bbd9948
blackkynaja <blackkynaja@gmail.com>
2022-02-24 01:16:31 -0800

    Create ClimbingStairsCS.swift

---
+++ Swift/CodingPractice/CodeSignal/InterviewPractice/ClimbingStairsCS.swift
@@ -0,0 +1,17 @@
+/*
+ https://app.codesignal.com/interview-practice/task/oJXTWuwEZiC6FTw3A/description
+ */
+
+class ClimbingStairsCS {
+    func climbingStairsHelper(n: Int) -> Int {
+        if n == 1 || n == 0 {
+            return 1
+        } else {
+            return climbingStairsHelper(n: n-1) + climbingStairsHelper(n: n-2)
+        }
+    }
+
+    func climbingStairs(n: Int) -> Int {
+        climbingStairsHelper(n: n)
+    }
+}
